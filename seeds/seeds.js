3af6852640354d232631b978840b51c651e85a46
Minh Thach <tatsumiikun1@gmail.com>
2022-10-27 11:44:51 -0400

    database&seeds

---
+++ seeds/seeds.js
@@ -0,0 +1,22 @@
+const sequelize = requiire('../config/connection');
+const { User, Review, Comment } = require('../models');
+
+const commentData = require('./commentData.json');
+const reviewData = require('./reviewData.Json');
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({force: true});
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Review.bulkCreate(reviewData);
+    await Comment.bulkCreate(commentData);
+
+    process.exit(0);
+}
+
+seedDatabase();
