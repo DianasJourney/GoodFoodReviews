7a379f7877cb29cf215815025ddd13668adfa2df
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-10-31 12:21:19 -0400

    refactored and commented reviewboard-routes

--- controllers/reviewboard-routes.js
+++ controllers/reviewboard-routes.js
@@ -1,32 +1,39 @@
-const router = require('express').Router()
-const { Review, User } = require('../models')
-const withAuth = require('../utils/auth')
+const router = require('express').Router();
+const { Review, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-//gets all of logged in users reviews in our review dashboard
-router.get('/', withAuth, async (req, res) => {
+/* Each route defined below will:
+ * Asynchronously GET all of this session users' reviews and 
+ * send the serialized data to the corresponding view.
+ */
+
+router.get('/', withAuth, async(req, res) => {
   try {
     const reviewData = await Review.findAll({
       where: {
         user_id: req.session.user_id
       }
-    })
+    });
 
-    const reviews = reviewData.map(review => review.get({ plain: true }))
-
-    res.render('reviewboard', { reviews, loggedIn: req.session.loggedIn })
+    if (reviewData) {
+      const reviews = reviewData.map(review => review.get({ plain: true }));
+      res.render('reviewboard', { reviews, loggedIn: req.session.loggedIn });
+    }
+    else {
+      res.status(404).end()
+    }
   } catch (err) {
     res.status(500).json(err)
   }
-})
+});
 
-router.get('/create', withAuth, (req, res) => {
-  console.log('reviewboard.get /create reached');
+router.get('/create', withAuth, async(req, res) => {
   res.render('create-post', {
     layout: 'main'
-  })
-})
+  });
+});
 
-router.get('/edit/:id', withAuth, async (req, res) => {
+router.get('/edit/:id', withAuth, async(req, res) => {
   try {
     const reviewData = await Review.findByPk(req.params.id);
     
@@ -35,7 +42,7 @@ router.get('/edit/:id', withAuth, async (req, res) => {
       res.render('edit-post', {
         layout: 'main',
         review
-      })
+      });
     } else {
       res.status(404).end()
     }
@@ -44,8 +51,7 @@ router.get('/edit/:id', withAuth, async (req, res) => {
     }
   });
 
-
-router.get('/delete/:id', withAuth, async (req, res) => {
+router.get('/delete/:id', withAuth, async(req, res) => {
   try {
   const reviewData = await Review.findByPk(req.params.id);
   
@@ -54,7 +60,7 @@ router.get('/delete/:id', withAuth, async (req, res) => {
     res.render('delete-post', {
       layout: 'main',
       review
-    })
+    });
   } else {
     res.status(404).end()
   }
@@ -63,4 +69,4 @@ router.get('/delete/:id', withAuth, async (req, res) => {
   }
 });
 
-module.exports = router
+module.exports = router;
